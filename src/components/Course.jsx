eaa2b3c5bf394345f1d9b98ad6f57d183fdfe2f1
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-08-18 09:32:52 +0330

    course header and course footer extracted

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -5,27 +5,38 @@ const Course = ({ course }) => {
     <div className="course">
       <img className="courseImage" src={course.imgUrl} alt="image" />
       <div className="courseDetail">
-        <div className="courseDetail__header">
-          <div className="courseDetail__name-essentials">
-            <h3 className="courseDetail__name">{course.title}</h3>
-            <span className="star">
-              <AiFillStar />
-              {course.rate}
-            </span>
-          </div>
-          <p className="couresDetail__descp">{course.description}</p>
-        </div>
-        <div className="courseDetail__footer">
-          <div className="tags">
-            {course.tags.map((tag) => (
-              <Badge key={tag} tag={tag} />
-            ))}
-          </div>
-          <div className="courseDetail__footer-essentials">
-            <p className="courseDetail__date">{course.date}</p>
-            <Badge tag={course.status} />
-          </div>
-        </div>
+        <CourseHeader course={course} />
+        <CourseFooter course={course} />
+      </div>
+    </div>
+  );
+};
+
+const CourseHeader = ({ course }) => {
+  return (
+    <div className="courseDetail__header">
+      <div className="courseDetail__name-essentials">
+        <h3 className="courseDetail__name">{course.title}</h3>
+        <span className="star">
+          <AiFillStar />
+          {course.rate}
+        </span>
+      </div>
+      <p className="couresDetail__descp">{course.description}</p>
+    </div>
+  );
+};
+const CourseFooter = ({ course }) => {
+  return (
+    <div className="courseDetail__footer">
+      <div className="tags">
+        {course.tags.map((tag) => (
+          <Badge key={tag} tag={tag} />
+        ))}
+      </div>
+      <div className="courseDetail__footer-essentials">
+        <p className="courseDetail__date">{course.date}</p>
+        <Badge tag={course.status} />
       </div>
     </div>
   );
